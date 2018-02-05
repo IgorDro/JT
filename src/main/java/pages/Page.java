cc5dadbc351822b399248c133019f7b4be47b021
IgorDro <forjob1111@yandex.ru>
2018-02-05 14:09:40 +0300

    VM options -D 1.0.2

--- src/main/java/pages/Page.java
+++ src/main/java/pages/Page.java
@@ -1,12 +1,13 @@
 package pages;
 
-import jdk.nashorn.internal.objects.NativeString;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
 
 
@@ -37,21 +38,11 @@ public abstract class Page {
   public static String getPropertyOrEnv(String name, String theDefault) {
     String theValue = toLowerCase(System.getProperty(name));
     if (theValue == null) {
-      //System.out.println("Свойство не найдено " + theValue);
-      theValue = System.getenv(name);
-     // if (theValue == null) {
-     //   System.out.println("Не удалось найти переменную, используйте значение по умолчанию " + theDefault);
         theValue = theDefault;
-    //  } else {
-      //  System.out.println("Используйте переменную, со значением " + theValue);
-     // }
     } else  if (theValue.equals("chrome")) {
-      //  System.out.println("Используйте свойств, со значением " + theValue);
-      return toLowerCase(theValue);
       } else {
-      //  System.out.println("Такого браузера нет или используется по умолчанию, используйте значение по умолчанию " + theDefault);
         theValue = theDefault;
       }
-    return toLowerCase(theValue);
+    return theValue;
   }
 }
