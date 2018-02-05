ba71601e0c6db00a9affd228a6d0a2a2e01d39a8
IgorDro <forjob1111@yandex.ru>
2018-02-05 11:09:23 +0300

    VM options -D

--- src/main/java/pages/Page.java
+++ src/main/java/pages/Page.java
@@ -1,25 +1,12 @@
 package pages;
 
-import com.sun.jna.Native;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
-
-import java.awt.*;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Properties;
-import java.util.Scanner;
-
-import static java.awt.SystemColor.textText;
-import static java.awt.SystemColor.windowText;
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
 
 
 public abstract class Page {
@@ -31,39 +18,16 @@ public abstract class Page {
   }
 
   public static WebDriver getDriver() {
-
-//    String line, br = "";
-//    try {
-//    Scanner scanner = new Scanner(new FileReader("C:\\Webdrivers\\StartNode.bat"));
-//    while(scanner.hasNextLine())
-//    {
-//      line = scanner.nextLine();
-//      if (line.contains("set bro="))
-//      {br = line.substring(8);
-//        break;}
-//    }} catch (IOException ex) {
-//      System.out.println("Что-то пошло не так");
-//    }
-
-
-    String theDefault = "firefox";
-//    Properties props = new Properties();
-//    props = System.getProperties();
-//    caps.setBrowserName(props.getProperty("browserName"));
-
+    String theDefault = "FIREFOX";
 
     if (driver == null)
       try {
         DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setBrowserName(System.getProperty("browserName"));
-//        caps.setCapability("browsername","chrome" );
         caps.setBrowserName(getPropertyOrEnv("browserName", theDefault));
         driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
       } catch (MalformedURLException ex) {
         System.out.println("Что-то пошло не так опять");
       }
-
-
     // driver = new ChromeDriver();
     //driver = new FirefoxDriver();
     return driver;
@@ -75,17 +39,17 @@ public abstract class Page {
       System.out.println("Свойство не найдено " + theValue);
       theValue = System.getenv(name);
       if (theValue == null) {
-        System.out.println("Не удалось найти переменную " + theValue + " используйте значение по умолчанию " + theDefault);
+        System.out.println("Не удалось найти переменную, используйте значение по умолчанию " + theDefault);
         theValue = theDefault;
       } else {
-        System.out.println("Используйте переменную " + theValue + " со значением " + theValue);
-    }
-    } else  if (theValue != "chrome" || theValue == "firefox" ) {
-      System.out.println("Такого браузера нет или используется по умолчанию " + theValue + " используйте значение по умолчанию " + theDefault);
+        System.out.println("Используйте переменную, со значением " + theValue);
+      }
+    } else if (theValue != "chrome" || theValue == "firefox") {
+      System.out.println("Такого браузера нет или используется по умолчанию, используйте значение по умолчанию " + theDefault);
       theValue = theDefault;
     } else {
-      System.out.println("Используйте свойство " + theValue + " со значением " + theValue);
+      System.out.println("Используйте свойств, со значением " + theValue);
     }
-    return theValue;
+    return toLowerCase(theValue);
   }
 }
